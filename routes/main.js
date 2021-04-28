487b48fb9935cdce308280eee44c4c0c3b3deeb7
Daniel-Oyebode <Dannyoyebode@gmail.com>
2021-04-28 17:50:16 +0100

    First Commit

---
+++ routes/main.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const controllers = require('../controllers')
+
+router.get('/', async (req, res, next) => {
+  const data = req.context
+
+  const itemCtr = controllers.item.instance()
+  data.Fruits = await itemCtr.get({ category: 'Fruits' })
+  data.Vegetables = await itemCtr.get({ category: 'Vegetables' })
+  data.Legumes = await itemCtr.get({ category: 'Legumes' })
+
+  res.render('home', req.context)
+})
+
+router.get('/blog', (req, res, next) => {
+  res.render('blog', req.context)
+})
+
+router.get('/items', async (req, res, next) => {
+  const filters = req.query
+  const itemCtr = controllers.item.instance()
+  const items = await itemCtr.get(filters)
+
+  res.json({
+    items,
+  })
+})
+
+router.post('/order', async (req, res, next) => {
+  const orderData = req.body
+  const orderCtr = controllers.order.instance()
+  const order = await orderCtr.post(orderData)
+  res.json(order)
+})
+
+module.exports = router
